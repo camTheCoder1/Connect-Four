1a5786a1373d4719e6b226abf73b40c32a919d78
Cameron  Morgan <cm09516@georgiasouthern.edu>
2019-10-04 14:58:43 -0400

    loop to check for winner going down

--- js/connect4.js
+++ js/connect4.js
@@ -5,4 +5,14 @@ function checkLine(cellA,cellB,cellC,cellD) {
     return ((cellA != 0) && (cellA ==cellB) && (cellA == cellC) && (cellA == cellD));
 }
 
+function checkWinner(check) {
+    // Check for winner going down
+    for ( row = 0; row < 3; row++)
+        for ( col = 0; col < 7; col++)
+            if (checkLine(check[row][col], check[row+1][col], check[row+2][col], check[row+3][col]))
+                return check[row][col];
+
+
+    return 0;
+}
 
