c36d17cf3c554d6e3961fd07246348591cc97654
Cameron  Morgan <cm09516@georgiasouthern.edu>
2019-10-04 14:05:16 -0400

    function to check if the first cell is not empty and find matching pieces

---
+++ js/connect4.js
@@ -0,0 +1,8 @@
+
+
+function checkLine(cellA,cellB,cellC,cellD) {
+    // Check first cell non-zero and all cells match
+    return ((cellA != 0) && (cellA ==cellB) && (cellA == cellC) && (cellA == cellD));
+}
+
+
