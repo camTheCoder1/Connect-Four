452cbfb7e1ff4ea5c2a591040e8c241542cc9fc4
Cameron  Morgan <cm09516@georgiasouthern.edu>
2019-10-08 10:40:21 -0400

    updated loop to check for vertical winner

--- js/connect4.js
+++ js/connect4.js
@@ -1,6 +1,6 @@
 const playerColors = {
     '1': 'black',
-    '-1': 'red',
+    '2': 'red',
     'null': 'white'
 };
 
@@ -8,14 +8,21 @@ const board, turn, win;
 
 function initGrid() {
     board = [
-        [null, null, null, null, null, null],
-        [null, null, null, null, null, null],
-        [null, null, null, null, null, null],
-        [null, null, null, null, null, null],
-        [null, null, null, null, null, null],
-        [null, null, null, null, null, null],
-        [null, null, null, null, null, null]
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
     ];
     turn = 1;
     win = null;
+}
+
+// Check for winner going down
+function checkVert(colArr) {
+    for (var row = 0; row < 3; row++) {
+        if ((colArr[row] + colArr[row + 1] + colArr[row + 2] + colArr[row + 3]) === 4) return colArr[row];
+    }
+    return null;
 }
