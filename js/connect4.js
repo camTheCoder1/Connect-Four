e589a9bf60a73294a087dc1ce3682c26969207cf
Cameron  Morgan <cm09516@georgiasouthern.edu>
2019-10-08 10:45:54 -0400

    updated all other loops to find winner

--- js/connect4.js
+++ js/connect4.js
@@ -18,11 +18,47 @@ function initGrid() {
     turn = 1;
     win = null;
 }
+function checkLine(cellA, cellB, cellC, cellD) {
+    // Check first cell non-zero and all cells match
+    return ((cellA != 0) && (cellA == cellB) && (cellA == cellC) && (cellA == cellD));
+}
 
-// Check for winner going down
-function checkVert(colArr) {
-    for (var row = 0; row < 3; row++) {
-        if ((colArr[row] + colArr[row + 1] + colArr[row + 2] + colArr[row + 3]) === 4) return colArr[row];
+function checkWinner(check) {
+    // Check for winner going down
+    for (row = 0; row < 3; row++) {
+        for (col = 0; col < 7; col++) {
+            if (checkLine(check[row][col], check[row + 1][col], check[row + 2][col], check[row + 3][col])) {
+                return check[row][col];
+
+            }
+        }
+        return null
     }
-    return null;
+    // Check for winner going right
+    for (row = 0; row < 6; row++){
+        for (col = 0; col < 4; col++){
+            if (checkLine(check[row][col], check[row][col + 1], check[row][col + 2], check[row][col + 3])){
+                return check[row][col];
+            }
+        }
+        return null
+    }
+    // Check for winner diagnally going down-right
+    for (row = 0; row < 3; row++){
+        for (col = 0; col < 4; col++){
+            if (checkLine(check[row][col], check[row + 1][col + 1], check[row + 2][col + 2], check[row + 3][col + 3])){
+                return check[row][col];
+            }
+        }
+        return null
+    }
+    // Check for winner diagnally going down-left
+    for (row = 3; row < 6; row++){
+        for (col = 0; col < 4; col++){
+            if (checkLine(check[row][col], check[row - 1][col + 1], check[row - 2][col + 2], check[row - 3][col + 3])){
+                return check[row][col];
+            }
+        }
+        return null;
+    }
 }
