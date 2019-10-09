05a56e38c9ef0d2558bbc88beaac7253979b819b
Cameron  Morgan <cm09516@georgiasouthern.edu>
2019-10-08 23:36:58 -0400

    added a how to play section

--- js/connect4.js
+++ js/connect4.js
@@ -22,32 +22,32 @@ var board, turn, win;
 
 var mess = document.getElementById('message');
 
-document.querySelector('table').addEventListener('click', function(e) {
+document.querySelector('table').addEventListener('click', function (e) {
     if (win) return;
     var colIdx = parseInt(e.target.getAttribute('data-col'));
     var column = board[colIdx];
     if (!column.includes(null)) return;
-column[column.indexOf(null)] = turn;
+    column[column.indexOf(null)] = turn;
     turn *= -1;
     win = getWinner();
     check();
 });
 
-document.querySelector('button').addEventListener('click', function(e) {
+document.querySelector('button').addEventListener('click', function (e) {
     initialize();
     check();
 });
 
 function check() {
-    board.forEach(function(colArr, idx) {
+    board.forEach(function (colArr, idx) {
         var cells = document.querySelectorAll(`[data-col="${idx}"]`)
-        for( var i = 0; i < cells.length; i++) {
+        for (var i = 0; i < cells.length; i++) {
             cells[5 - i].style.backgroundColor = playerColors[board[idx][i]];
         }
         if (win) {
-        mess.innerHTML = `${playerColors[win].toUpperCase()} Wins!`;
+            mess.innerHTML = `${playerColors[win].toUpperCase()} Wins!`;
         } else {
-        mess.innerHTML = ` ${playerColors[turn].toUpperCase()}'s Turn!`;
+            mess.innerHTML = ` ${playerColors[turn].toUpperCase()}'s Turn!`;
         }
     });
 }
@@ -62,61 +62,61 @@ function getWinner() {
 }
 
 function getColumnWinner(colIdx) {
-    var winner; 
+    var winner;
     var colArr = board[colIdx];
     winner = checkVert(colArr);
-    if (winner){
+    if (winner) {
         return winner;
-    } 
+    }
     winner = checkHoriz(colIdx);
-    if (winner){
-         return winner;
-        }
+    if (winner) {
+        return winner;
+    }
     winner = checkDiag(colIdx, 1);
-    if (winner){
+    if (winner) {
         return winner;
-    } 
+    }
     winner = checkDiag(colIdx, -1);
-    if (winner){
+    if (winner) {
         return winner;
-    } 
+    }
     return null;
 }
 
-    // Check for winner going down
+// Check for winner going down
 function checkVert(colArr) {
     for (var row = 0; row < 3; row++) {
-        if (Math.abs(colArr[row] + colArr[row + 1] + colArr[row + 2] + colArr[row + 3]) === 4){
+        if (Math.abs(colArr[row] + colArr[row + 1] + colArr[row + 2] + colArr[row + 3]) === 4) {
             return colArr[row];
-        } 
+        }
     }
     return null;
 }
 
-    // Check for winner going right
+// Check for winner going right
 function checkHoriz(colIdx) {
-    if (colIdx > 3){
-      return null;  
-    } 
+    if (colIdx > 3) {
+        return null;
+    }
     for (var row = 0; row < 7; row++) {
-        if (Math.abs(board[row][colIdx] + board[row][colIdx + 1] + board[row][colIdx + 2]+ board[row][colIdx + 3]) === 4){
+        if (Math.abs(board[row][colIdx] + board[row][colIdx + 1] + board[row][colIdx + 2] + board[row][colIdx + 3]) === 4) {
             return board[row][colIdx];
-        } 
+        }
     }
     return null;
 }
 
-    //Check for winner going diagnal
+//Check for winner going diagnal
 function checkDiag(colIdx, dia) {
     if (colIdx > 3) {
-    return null;
+        return null;
     }
     for (var row = 0; row < 7; row++) {
-        if ((dia === 1 && row > 2) || (dia === -1 && row > 3)){
+        if ((dia === 1 && row > 2) || (dia === -1 && row > 3)) {
             break;
-        } 
+        }
         if (Math.abs(board[colIdx][row] + board[colIdx + 1][row + dia] + board[colIdx + 2][row + dia * 2] + board[colIdx + 3][row + dia * 3]) === 4) {
-        return board[colIdx][row];
+            return board[colIdx][row];
         }
     }
     return null;
